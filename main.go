431a1d644cb06b63b30174c938b4ccef9f7e7fb0
Octane0411 <wdznb1@gmail.com>
2022-04-07 12:08:00 +0800

    添加Dockerfile

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	e := gin.Default()
+	e.Handle("GET", "/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"msg": "hello"})
+	})
+	e.Run(":8080")
 }
